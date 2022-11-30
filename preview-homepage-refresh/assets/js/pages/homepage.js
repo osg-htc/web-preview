64a41dce812033455b97f5de64a79731c8b97b50
Cannon Lock <clock@wisc.edu>
2022-11-30 14:04:41 -0600

    Nuke Unused Previews

--- preview-homepage-refresh/assets/js/pages/homepage.js
+++
@@ -1,5 +0,0 @@
-import { GlobeVisualization } from "../components/GlobeVisualization.js";
-
-const globe_visualization = new GlobeVisualization("globe-visualization")
-
-export default globe_visualization
