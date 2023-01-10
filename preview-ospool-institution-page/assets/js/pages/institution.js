2abd0d394e557799478b2a14bffcfa0e4eeb38f8
Automatic preview publish <help@osg-htc.org>
2023-01-10 17:12:36 +0000

    Build Branch Website

---
+++ preview-ospool-institution-page/assets/js/pages/institution.js
@@ -0,0 +1,22 @@
+import FacilityPage, { getFacilityEsData, getTopologyData } from "../facility-table.js";
+
+const GRAFANA_PROJECT_BASE_URL = "https://gracc.opensciencegrid.org/d-solo/axV4YtN4k/facility-public"
+
+async function getData() {
+    if (!getData.data) {
+        let elasticSearchData = await getFacilityEsData()
+        let topologyData = await getTopologyData()
+
+        // Combine the data sets on facility name
+        getData.data = Object.entries(topologyData).reduce((p, [k, v]) => {
+            if (k in elasticSearchData) {
+                p[k] = {...elasticSearchData[k], ...topologyData[k]}
+            }
+            return p
+        }, {})
+    }
+
+    return getData.data
+}
+
+const facility_page = new FacilityPage(getData, GRAFANA_PROJECT_BASE_URL)
